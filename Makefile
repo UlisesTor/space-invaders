680a3bb2df3d0173130db81251a887dacbbd2b00
UlisesTor <159966473+UlisesTor@users.noreply.github.com>
2024-04-17 00:22:54 +0000

    Version 3

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 bin/test : src/tui_test.cpp
      g++ src/tui_test.cpp -o bin/test -std=c++2a -lftxui-screen -lftxui-component -lftxui -dom
 
+bin/space: src/main.cpp
+     g++ src/main.cpp.cpp -o bin/space -std=c++2a -lftxui-screen -lftxui-component -lftxui -dom     
+
 
  run : bin/test
      ./bin/test
+run     
 
 	  
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
-#incluide  <iostream>
+#include  <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/scren.hpp>
+using namespace std;
+using namespace ftxui;
 
-codespace::std 
+
+//(Generador de imagen)
 int main(int argc, char const *argv[])
 {
+    Element lienzo = hbox({
+        spinner(1,1)     bold 
+    });
+
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Full()
+    );
+
+
+    Render(pantalla,lienzo);//Tomar lo que esta dentro del lienzo y dibujalo dentro de la pantalla
+    pantalla.Print();
+    pantalla.ResentPosition();
+
+    this_thread::sleep_for(0.1s);
+
 
     return 0;
 }
